224c2f21cadc7e0fc6087eba6e4fe616c57cf6de
Erick5933 <125570530+Erick5933@users.noreply.github.com>
2025-07-10 00:46:55 -0500

    Use localdate as default for fecha_registro field
    Changed the default value of the 'fecha_registro' field in the Usuario model from timezone.now to localdate to ensure the date is stored in the local time zone.

--- core/models/usuario.py
+++ core/models/usuario.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.core.validators import RegexValidator
 from django.utils import timezone
 from .barrio import Barrio
+from django.utils.timezone import localdate
 
 class Usuario(models.Model):
     TIPO_USUARIO_CHOICES = [
@@ -14,7 +15,7 @@ class Usuario(models.Model):
     contraseña = models.CharField(max_length=128)
     tipo_usuario = models.CharField(max_length=20, choices=TIPO_USUARIO_CHOICES)
     barrio = models.ForeignKey(Barrio, on_delete=models.SET_NULL, null=True, blank=True, related_name="usuarios")
-    fecha_registro = models.DateField(default=timezone.now)
+    fecha_registro = models.DateField(default=localdate)
     estado = models.BooleanField(default=True)
     nombres = models.CharField(max_length=100)
     apellidos = models.CharField(max_length=100)
